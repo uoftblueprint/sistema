eaf74be09e82e112f0b7ca1cd278386b20c089ee
ramyzhang <ramyjzh@gmail.com>
2022-11-11 22:09:04 -0500

    android emulator is working, pog

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -4,16 +4,38 @@
  *
  * @format
  */
- const path = require('path')
+const path = require('path');
+const exclusionList = require("metro-config/src/defaults/exclusionList");
+const {
+    getMetroTools,
+    getMetroAndroidAssetsResolutionFix,
+} = require("react-native-monorepo-tools");
+const monorepoMetroTools = getMetroTools();
+const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix();
 
 module.exports = {
   projectRoot: path.resolve(__dirname, '../../'),
   transformer: {
+    publicPath: androidAssetsResolutionFix.publicPath,
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
+  },
+  server: {
+    // ...and to the server middleware.
+    enhanceMiddleware: (middleware) => {
+      return androidAssetsResolutionFix.applyMiddleware(middleware);
+    },
+  },
+  // Add additional Yarn workspace package roots to the module map.
+  // This allows importing importing from all the project's packages.
+  watchFolders: monorepoMetroTools.watchFolders,
+  resolver: {
+    // Ensure we resolve nohoist libraries from this directory.
+    blockList: exclusionList(monorepoMetroTools.blockList),
+    extraNodeModules: monorepoMetroTools.extraNodeModules,
   },
 };
