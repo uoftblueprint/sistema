20293314b3e6a0971b352eadf0f6567a37e0d0e3
MinGi-K <mingikwon1106@gmail.com>
2022-11-24 00:38:44 -0500

    added more comment to code

--- packages/mobile/src/components/Header.js
+++ packages/mobile/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-  StyleSheet,
-  SafeAreaView,
-  View,
-  Text,
-  TouchableOpacity,
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity,
 } from 'react-native';
 import SistemaLogo from '../../assets/SistemaLogo';
 import HomeIcon from '../../assets/HomeIcon';
@@ -15,51 +15,56 @@ import OptionIcon from '../../assets/OptionIcon';
 const height = Math.floor(Dimensions.get('window').height / 10);
 
 const Header = ({ navigation }) => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.settingContainer}>
-        <View style={styles.settingAdjustment}>
-          <OptionIcon />
-        </View>
+    return (
+        <SafeAreaView style={styles.container}>
 
-        <View style={{ flex: 1 }} />
-      </TouchableOpacity>
+            {/* touchableopacity wrapper for setting icon */}
+            <TouchableOpacity style={styles.settingContainer}>
 
-      <View style={styles.imageContainer}>
-        <SistemaLogo />
-      </View>
+                <View style={styles.settingAdjustment}>
+                    <OptionIcon />
+                </View>
 
-      <View style={{ flex: 1 }} />
-    </SafeAreaView>
-  );
+                {/* view created to shift settings icon slightly up */}
+                <View style={{ flex: 1 }} />
+            </TouchableOpacity>
+
+            <View style={styles.imageContainer}>
+                <SistemaLogo />
+            </View>
+            
+            {/* This view created to center image container in 1: 5 : 1 ratio */}
+            <View style={{ flex: 1 }} />
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row-reverse',
-    height: height,
-  },
-  imageContainer: {
-    //container for the sistema logo
-    flex: 5,
-    alignContent: 'center',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  settingContainer: {
-    //container for setting icon
-    flex: 1,
-    height: '100%',
-    flexDirection: 'column'
-  },
-  settingAdjustment: {
-    //for adjusting icon up slightly inside settingContainer
+    container: {
+        flexDirection: 'row-reverse',
+        height: height,
+    },
+    imageContainer: {
+        //container for the sistema logo
+        flex: 5,
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    settingContainer: {
+        //container for setting icon
+        flex: 1,
+        height: '100%',
+        flexDirection: 'column'
+    },
+    settingAdjustment: {
+        //for adjusting icon up slightly inside settingContainer
 
-    flex: 8,
-    alignContent: 'center',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+        flex: 8,
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
 
 export default Header;
