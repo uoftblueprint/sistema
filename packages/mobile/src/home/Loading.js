18a7bc6c373fd6616cfa6f895cb6bd8d8ebb138a
Helena Glowacki <helena.glowacki@mail.utoronto.ca>
2022-12-02 20:41:34 -0600

    fix(loading.js): fixed trailing commas
    Removed trailing commas from Loading.js StyleSheets
    
    SIS #026

--- packages/mobile/src/home/Loading.js
+++ packages/mobile/src/home/Loading.js
@@ -1,51 +1,47 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet, View} from 'react-native';
+import { SafeAreaView, Text, StyleSheet, View } from 'react-native';
 import SistemaLogo from '../../assets/SistemaLogoLoading';
 
-const Loading = ()  => {
+const Loading = () => {
   return (
-      <SafeAreaView style={styles.background}>
-        <View style={styles.imageContainer}> 
-          <SistemaLogo/>
-          </View>
-        <Text style={styles.appname}>
-          App Name
-        </Text>
-        <Text style={styles.visionStatement}>
-          Vision Statement
-        </Text>
-      </SafeAreaView>      
+    <SafeAreaView style={styles.background}>
+      <View style={styles.imageContainer}>
+        <SistemaLogo />
+      </View>
+      <Text style={styles.appname}>App Name</Text>
+      <Text style={styles.visionStatement}>Vision Statement</Text>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
-      backgroundColor: '#FFFAF5',
-      height: '100%',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
+    backgroundColor: '#FFFAF5',
+    height: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  imageContainer:{
+  imageContainer: {
     height: 99,
-    width: 171,
-},
-//add 'Poppins' font
+    width: 171
+  },
+  //add 'Poppins' font
   appname: {
-        paddingTop: '4%',
-        color: '#000000',
-        fontSize: 42,
-        fontWeight: '400',
-        letterSpacing: 0.05,
-        lineHeight: 63,
-        },
-  visionStatement:{
-        paddingTop: '4%',
-        color: '#000000',
-        fontSize: 23,
-        fontWeight: '400',
-        letterSpacing: 0.05,
-        lineHeight: 34,
-  }
-   })
+    paddingTop: '4%',
+    color: '#000000',
+    fontSize: 42,
+    fontWeight: '400',
+    letterSpacing: 0.05,
+    lineHeight: 63
+  },
+  visionStatement: {
+    paddingTop: '4%',
+    color: '#000000',
+    fontSize: 23,
+    fontWeight: '400',
+    letterSpacing: 0.05,
+    lineHeight: 34
+  },
+});
 export default Loading;
