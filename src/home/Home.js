24685908d457c644c7bee1a7a9bc2008fb916f06
Harmit Goswami <90732381+harmitgoswami@users.noreply.github.com>
2022-12-10 00:30:00 -0500

    fix(home.js, recentcard.js): added comment suggestions to Home & RecentCard (less hardcoded)
    re #022

--- src/home/Home.js
+++ src/home/Home.js
@@ -5,15 +5,16 @@ import RecentCard from '../home/components/RecentCard';
 const Home = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.background}>
-      <SafeAreaView style={{marginHorizontal: 10, flex: 1}}>
-        <Text style={styles.title}>Recently added activity cards</Text>
-
+      <SafeAreaView>
         <SafeAreaView>
-          <RecentCard style={{flex: 3}}></RecentCard>
-          <RecentCard style={{flex: 3}}></RecentCard>
-          <RecentCard style={{flex: 3}}></RecentCard>
+          <Text style={styles.title}>Recently added activity cards</Text>
         </SafeAreaView>
 
+        <SafeAreaView style={{ height: '100%' }}>
+          <RecentCard />
+          <RecentCard />
+          <RecentCard />
+        </SafeAreaView>
       </SafeAreaView>
     </SafeAreaView>
   );
@@ -22,15 +23,16 @@ const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
   background: {
     backgroundColor: '#FFFAF5',
-    height: '100%',
+    height: '100%'
   },
   title: {
     color: '#453E3D',
     fontWeight: '700',
-    fontSize: 20,
+    fontSize: 25,
     marginHorizontal: 30,
-    letterSpacing: 0.3
-  },
+    letterSpacing: 0.3,
+    width: '100%'
+  }
 });
 
 export default Home;
--- src/home/components/RecentCard.js
+++ src/home/components/RecentCard.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  ScrollView,
+  Dimensions
+} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const RecentCard = ({ navigation }) => {
   return (
-    <SafeAreaView style={{alignItems: 'center', justifyContent: 'center'}}>
-
-        <SafeAreaView style={styles.box}>
-
-          <ScrollView>
-            {/* <Text>CARD GOES HERE</Text> */}
-          </ScrollView>
-
-        </SafeAreaView>
-
+    <SafeAreaView style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <SafeAreaView style={styles.box}>
+        <ScrollView>{/* <Text>CARD GOES HERE</Text> */}</ScrollView>
+      </SafeAreaView>
     </SafeAreaView>
   );
 };
@@ -27,13 +30,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     overflow: 'hidden',
     marginVertical: 15,
-    width: 332,
-    height: 187
+    width: '87%',
+    height: windowHeight * 0.25
   },
   scrollview: {
     backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   }
 });
 
