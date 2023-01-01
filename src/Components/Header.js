8cdc7b84b1e7d3ef6bff636122d59d05cb7ce153
MinGi-K <mingikwon1106@gmail.com>
2023-01-01 17:30:01 +0900

    squash

---
+++ src/Components/Header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity,
+} from 'react-native';
+import SistemaLogo from '../../assets/SistemaLogo';
+import HomeIcon from '../../assets/HomeIcon';
+import OptionIcon from '../../assets/OptionIcon';
+
+const Header = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+
+                <View style={styles.logoContainer}>
+                    <SistemaLogo/>
+                </View>
+                
+                {/* touchableopacity wrapper for setting icon */}
+                <TouchableOpacity style={styles.settingContainer}>
+                    <OptionIcon style={styles.settingIcon}/>
+                </TouchableOpacity>
+                
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        height: '10%',
+    },
+    header:{
+        height: '100%',
+        alignItems: 'center',
+        backgroundColor:'blue',
+        flexDirection: 'row-reverse'
+    },
+    logoContainer: {
+        position: 'absolute', 
+        top: 0, left: 0, right: 0, bottom: 0, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    },
+    settingContainer: {
+        //container for setting icon
+        backgroundColor: 'pink',
+        height: '100%',
+        alignItems:'center',
+        justifyContent: 'center',
+    },
+    settingIcon:{
+        margin: 'auto'
+    }
+});
+
+export default Header;
