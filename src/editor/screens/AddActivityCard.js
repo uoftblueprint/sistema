c17426369565f5a099d04d7c9d9af2a813d257cb
MinGi-K <mingikwon1106@gmail.com>
2023-01-15 01:17:22 -0500

    removed comma

--- src/editor/screens/AddActivityCard.js
+++ src/editor/screens/AddActivityCard.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import {
-    SafeAreaView,
-    View,
-    Text,
-    StyleSheet,
-    TouchableOpacity,
-    useState
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  useState
 } from 'react-native';
 import TagFilter from '../components/TagFilter';
 import BackArrow from '../../../assets/backArrow.svg';
@@ -14,84 +14,83 @@ import { mdiMagnify } from '@mdi/js';
 import { isSearchBarAvailableForCurrentPlatform } from 'react-native-screens';
 
 const TAGS = [
-    'Warm Up',
-    'No Equipment',
-    'Beginner',
-    'Rhythm',
-    'Note Reading',
-    'Group Activity',
-    'Quick',
-    'Scale'
+  'Warm Up',
+  'No Equipment',
+  'Beginner',
+  'Rhythm',
+  'Note Reading',
+  'Group Activity',
+  'Quick',
+  'Scale'
 ];
 
 const AddActivityCard = function () {
-    const [searchQuery, setSearchQuery] = React.useState('');
+  const [searchQuery, setSearchQuery] = React.useState('');
 
-    const onChangeSearch = query => setSearchQuery(query);
+  const onChangeSearch = query => setSearchQuery(query);
 
-    return (
-        <SafeAreaView style={styles.container}>
-           <View style={{paddingHorizontal:'3%'}}>
-           <TouchableOpacity style={styles.backButton}>
-                <BackArrow height={15} width={15} />
-                <Text style={styles.backText}> Back </Text>
-            </TouchableOpacity>
-            <Text style={styles.header}> Main Lesson Activity Card </Text>
-            <Text style={styles.tags}> Tags: </Text>
-            <View style={styles.tagContainer}>
-                {TAGS.map((tag, index) => (
-                    <TagFilter key={index} tagContent={tag} />
-                ))}
-            </View>
-            <Searchbar
-                placeholder="Search by title or keyword"
-                onChangeText={onChangeSearch}
-                value={searchQuery}
-                icon={require('../../../assets/search.png')}
-                style={{ backgroundColor: 'white', marginTop: '3%' }}
-            />
-           </View>
-            
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{ paddingHorizontal: '3%' }}>
+        <TouchableOpacity style={styles.backButton}>
+          <BackArrow height={15} width={15} />
+          <Text style={styles.backText}> Back </Text>
+        </TouchableOpacity>
+        <Text style={styles.header}> Main Lesson Activity Card </Text>
+        <Text style={styles.tags}> Tags: </Text>
+        <View style={styles.tagContainer}>
+          {TAGS.map((tag, index) => (
+            <TagFilter key={index} tagContent={tag} />
+          ))}
+        </View>
+        <Searchbar
+          placeholder="Search by title or keyword"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+          icon={require('../../../assets/search.png')}
+          style={{ backgroundColor: 'white', marginTop: '3%' }}
+        />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        width: '100%',
-    },
-    tagContainer: {
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-    },
-    header: {
-        color: 'black',
-        fontSize: 22,
-        fontFamily: 'Poppins-ExtraBold',
-        marginTop: '5%'
-    },
-    tags: {
-        color: 'black',
-                fontSize: 18,
-                fontFamily: 'Mulish-Regular',
-                marginTop: '-1%',
-                marginBottom: '2%'
-    },
-    backText: {
-        color: 'black',
-        fontSize: 18,
-        fontFamily: 'Roboto-Regular'
-    },
-    backButton: {
-        flexWrap: 'wrap',
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginTop: '30%',
-        width: '20%',
-        justifyContent: 'space-evenly'
-    }
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%'
+  },
+  tagContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  header: {
+    color: 'black',
+    fontSize: 22,
+    fontFamily: 'Poppins-ExtraBold',
+    marginTop: '5%'
+  },
+  tags: {
+    color: 'black',
+    fontSize: 18,
+    fontFamily: 'Mulish-Regular',
+    marginTop: '-1%',
+    marginBottom: '2%'
+  },
+  backText: {
+    color: 'black',
+    fontSize: 18,
+    fontFamily: 'Roboto-Regular'
+  },
+  backButton: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: '30%',
+    width: '20%',
+    justifyContent: 'space-evenly'
+  }
 });
 
 export default AddActivityCard;
