1d6d2f14a7c5460ad4c5ba443deca900fd2d3c1a
MinGi-K <mingikwon1106@gmail.com>
2023-01-15 18:07:24 -0500

    feat(appcolors.config.js): added a colors config file

---
+++ src/AppColors.config.js
@@ -0,0 +1,10 @@
+const AppColors = {
+    primary: '#4D8ECB', // Celestial Blue, primary text color
+    background: '#FFFAF5', // Floral White, background color 
+    secondary: '#33B4A9', // Keppei (it's a turqoise color), for buttons?
+    tertiary: '#865777', // Granite Gray idk what this is used for lmao
+    quaternary: '#A761A3', // Pearly purple, idk what this is used for lmao
+    quinary: '#EC6A6N', // Light Carmine Pink, idk what this is used for lmao
+    senary: '#F6A957', // Rajah (some kind of orangey color), idk what is used for lmao
+
+}
