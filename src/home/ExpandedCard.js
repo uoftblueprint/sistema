5fa1cc8c63803f80aeca9d0f7584c0ede673dfba
Harmit Goswami <90732381+harmitgoswami@users.noreply.github.com>
2023-01-19 20:45:08 -0500

    feat(expandedcard, home, homenavigator, app): added expandedcard stack to Home.js, with navigation
    re #048

---
+++ src/home/ExpandedCard.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+import AddButton from './components/AddToLessonButton';
+import BackArrow from '../../assets/BackArrow';
+import Header from '../Components/Header';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const ExpandedCard = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.background}>
+      <Header showBackButton={true} />
+
+      <TouchableOpacity
+        style={styles.backArrowContainer}
+        onPress={() => navigation.goBack()}>
+        <BackArrow style={styles.backArrow} />
+      </TouchableOpacity>
+
+      <ScrollView>
+        <SafeAreaView
+          style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={styles.title}> </Text>
+          {/* <Text style={styles.title}> props.cardTitle </Text> */}
+
+          <SafeAreaView style={styles.box}>
+            {/* CARD CONTENT GOES HERE */}
+          </SafeAreaView>
+
+          <AddButton />
+        </SafeAreaView>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#FFFAF5',
+    height: '100%'
+  },
+  box: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    marginTop: 15,
+    width: '75%',
+    height: windowHeight * 0.55
+  },
+  scrollview: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    color: '#453E3D',
+    fontSize: 20,
+    fontWeight: '700',
+    width: '60%',
+    textAlign: 'center',
+    marginBottom: 40
+  },
+  backArrowContainer: {
+    position: 'absolute',
+    top: 30,
+    bottom: 0,
+    left: 25,
+    right: 0
+  },
+  backArrow: {
+    fill: '#222222'
+  }
+});
+
+export default ExpandedCard;
--- src/home/components/RecentCard.js
+++ src/home/components/RecentCard.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {
   StyleSheet,
@@ -15,6 +16,14 @@ const RecentCard = ({ navigation }) => {
     <SafeAreaView style={{ alignItems: 'center', justifyContent: 'center' }}>
       <SafeAreaView style={styles.box}>
         <ScrollView>{/* <Text>CARD GOES HERE</Text> */}</ScrollView>
+
+        <SafeAreaView style={styles.titleBar}>
+          <SafeAreaView style={{ marginHorizontal: 20 }}>
+            <Text style={styles.title} numberOfLines={1}>
+              Listening - Knowledge - Listening Spinners
+            </Text>
+          </SafeAreaView>
+        </SafeAreaView>
       </SafeAreaView>
     </SafeAreaView>
   );
@@ -37,6 +46,20 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  titleBar: {
+    borderBottomLeftRadius: 18,
+    borderBottomRightRadius: 18,
+    width: '100%',
+    height: '19%',
+    backgroundColor: '#4D8ECB'
+  },
+  title: {
+    color: '#FFFFFF',
+    width: '100%',
+    fontSize: 17,
+    marginTop: 7,
+    fontStyle: 'italic'
   }
 });
 
