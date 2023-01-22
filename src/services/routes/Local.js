a902aa82f0938c9cad4d663fe0e297666605cc58
ramyzhang <ramyjzh@gmail.com>
2023-01-22 04:50:17 -0500

    build: installed rnfs and added sample readdir func
    BREAKING CHANGE: potentially will not build!

---
+++ src/services/routes/Local.js
@@ -0,0 +1,26 @@
+// require the module
+var RNFS = require('react-native-fs');
+
+// get a list of files and directories in the main bundle
+RNFS.readDir(RNFS.DocumentDirectoryPath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+  .then((result) => {
+    console.log('GOT RESULT', result);
+
+    // stat the first file
+    return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+  })
+  .then((statResult) => {
+    if (statResult[0].isFile()) {
+      // if we have a file, read it
+      return RNFS.readFile(statResult[1], 'utf8');
+    }
+
+    return 'no file';
+  })
+  .then((contents) => {
+    // log the file contents
+    console.log(contents);
+  })
+  .catch((err) => {
+    console.log(err.message, err.code);
+  });
