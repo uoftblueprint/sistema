fd67d51807ce90880ecc65b8d4dcedfeec8f9839
Baker Jackson <bakerwjackson@gmail.com>
2023-01-29 17:53:45 -0500

    feat(sistemabutton.js): create component for blue/purple buttons

---
+++ src/Components/SistemaButton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+const Button = props => {
+  switch (props.color) {
+    case 'blue':
+      styles.buttonContainer.backgroundColor = '#B8CFE4';
+      break;
+    case 'purple':
+      styles.buttonContainer.backgroundColor = '#68577760';
+      break;
+    default:
+      styles.buttonContainer.backgroundColor = '#68577760';
+      break;
+  }
+  return (
+    <TouchableOpacity style={styles.buttonContainer} onPress={props.onPress}>
+      {props.children}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '',
+    borderWidth: 0.77,
+    borderRadius: 6,
+    borderColor: '#453E3D',
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    alignSelf: 'center'
+  }
+});
+
+export default Button;
