e5ea80644517c57d68ab8f81c51337b224e30b1f
Helena Glowacki <helena.glowacki@mail.utoronto.ca>
2023-01-29 17:27:53 -0600

    fix: fix screen maybe
    changed auto and safe are view layer

--- src/editor/screens/LessonPlanMenuOverlay.js
+++ src/editor/screens/LessonPlanMenuOverlay.js
@@ -4,22 +4,19 @@ import OptionsMenu from '../components/OptionsMenu.js';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-
 const LessonPlanMenuOverlay = ({ navigation, route }) => {
   const isLessonPlanEditor = route.params.isLessonPlanEditor;
   const lessonPlanName = route.params.lessonPlanName;
 
   return (
-    <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
-      <SafeAreaView>
-        <SafeAreaView style={styles.overlay}>
+    <TouchableWithoutFeedback onPress={() => navigation.goBack()} >
+      <SafeAreaView  style={styles.overlay}>
           <OptionsMenu
             isLessonPlanEditor={isLessonPlanEditor}
             lessonPlanName={lessonPlanName}
             navigation={navigation}
             style={styles.menu}
           />
-        </SafeAreaView>
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
@@ -29,7 +26,7 @@ const styles = StyleSheet.create({
   overlay: {
     alignItems: 'center',
     width: '100%',
-    height: 'auto',
+    height: '100%',
     backgroundColor: 'rgba(217,217,217, 0.8)',
     justifyContent: 'flex-end'
   },
