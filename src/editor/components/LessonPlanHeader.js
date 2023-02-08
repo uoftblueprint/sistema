b3e65d6e25680318e562b0dca4d07629ea001e77
Kurtis Law <kurtislaww@gmail.com>
2023-02-08 02:12:33 -0500

    feat(lesson plan header): edit icon changes focus state

--- src/editor/components/LessonPlanHeader.js
+++ src/editor/components/LessonPlanHeader.js
@@ -1,26 +1,23 @@
 import React from 'react-native';
-import EditIcon from '../../../assets/edit.svg';
 import BackArrow from '../../../assets/backArrow.svg';
 import Menu from '../../../assets/menu.svg';
-
-import LessonPlanName from './LessonPlanName.js';
+import LessonPlanName from './LessonPlanName';
 
 import { StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
 
-const LessonPlanHeader = () => (
-  <SafeAreaView style={styles.container}>
-    <TouchableOpacity>
-      <BackArrow />
-    </TouchableOpacity>
-    <LessonPlanName subtitle={'LessonPlanHeader'} />
-    <TouchableOpacity>
-      <EditIcon onClick />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <Menu />
-    </TouchableOpacity>
-  </SafeAreaView>
-);
+const LessonPlanHeader = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity>
+        <BackArrow />
+      </TouchableOpacity>
+      <LessonPlanName />
+      <TouchableOpacity>
+        <Menu />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
@@ -28,13 +25,6 @@ const styles = StyleSheet.create({
     marginHorizontal: 33,
     justifyContent: 'space-between',
     alignItems: 'center'
-  },
-  title: {
-    color: '#000',
-    fontSize: 24,
-    fontWeight: 'bold',
-    paddingRight: 15,
-    letterSpacing: 0.3
   }
 });
 
--- src/editor/components/LessonPlanName.js
+++ src/editor/components/LessonPlanName.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import LessonPlanNameInput from './LessonPlanNameInput';
+import { SafeAreaView, StyleSheet, TextInput, Text, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import EditIcon from '../../../assets/edit.svg';
 
 const LessonPlanName = () => {
+  const [isEditable, setIsEditable] = useState(false);
+  const [lessonPlanName, setLessonPlanName] = useState('');
+
+  useEffect(() => {
+    const todayDate = new Date().toLocaleDateString('en-us', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    });
+    setLessonPlanName(todayDate);
+  }, []);
+
   return (
-    <SafeAreaView style={styles.sectionContainer}>
-      <View>
-        <LessonPlanNameInput placeholder={todayDate} />
-      </View>
+    <SafeAreaView style={styles.SectionStyle}>
+      {isEditable ? (
+        <TextInput
+          style={styles.input}
+          value={lessonPlanName}
+          onChangeText={newText => {
+            setLessonPlanName(newText);
+          }}
+          onBlur={() => {
+            setIsEditable(false);
+          }}
+          autoFocus={true}
+        />
+      ) : (
+        <Text style={styles.input} numberOfLines={1}>
+          {lessonPlanName}
+        </Text>
+      )}
+      <TouchableOpacity>
+        <EditIcon
+          onPress={() => {
+            setIsEditable(true);
+          }}
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
-const todayDate = new Date().toLocaleDateString('en-us', {
-  year: 'numeric',
-  month: 'short',
-  day: 'numeric'
-});
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginBottom: 0
+  input: {
+    flex: 1,
+    letterSpacing: 0.3,
+    paddingLeft: 12,
+    paddingRight: 15,
+    fontSize: 24,
+    color: '#000'
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFAF5',
+    height: 49,
+    width: 280,
+    color: '#000',
+    borderColor: 'transparent',
+    borderRadius: 7.69,
+    paddingLeft: '3%'
   }
 });
 
