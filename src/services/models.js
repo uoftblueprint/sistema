af5afa49740cbb45c00828927b648022d0eac410
MinGi-K <mingikwon1106@gmail.com>
2023-02-07 21:09:06 -0500

    removed duplicate export class Access Token

--- src/services/models.js
+++ src/services/models.js
@@ -11,19 +11,6 @@ export class AccessToken {
     this.type = token_type;
   }
 }
-/**
- * Access token infomation from getOAuthToken
- * @property {string} token Access token to use in Drive API requests
- * @property {number} expiryTime Local time in seconds at which token expires
- * @property {string} [type=Bearer] Default tokn type is always 'Bearer'
- */
- export class AccessToken {
-  constructor(access_token, expires_in, token_type = 'Bearer') {
-    this.token = access_token;
-    this.expiryTime = expires_in;
-    this.type = token_type;
-  }
-}
 
 /**
  * @property {string} name Name of the lesson plan
