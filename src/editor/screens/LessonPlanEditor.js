c3aad990436a157f7c7db30ef99e8d079497d4c5
ramyzhang <ramyjzh@gmail.com>
2023-02-12 20:53:14 -0500

    fix(lessonplaneditor.js): fixed onblur issue on ios

--- src/editor/screens/LessonPlanEditor.js
+++ src/editor/screens/LessonPlanEditor.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import LessonPlanHeader from '../components/LessonPlanHeader.js';
-import { StyleSheet, SafeAreaView, View, ScrollView } from 'react-native';
+import { StyleSheet, SafeAreaView, View, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import LessonSection from '../components/LessonSection.js';
 import LessonPlanNotes from '../components/LessonPlanNotes.js';
 import SaveButton from '../components/SaveButton.js';
@@ -10,17 +10,19 @@ const LessonPlanEditor = ({ navigation }) => {
     <SafeAreaView style={{ backgroundColor: '#FFFAF5' }}>
       <LessonPlanHeader />
       <ScrollView style={styles.scrollView}>
-        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
-          <LessonSection sectionType={'warmUp'} subtitle={'Warm Up'} />
-          <LessonSection sectionType={'mainLesson'} subtitle={'Main Lesson'} />
-          <LessonSection sectionType={'coolDown'} subtitle={'Cool Down'} />
-          <LessonPlanNotes
-            sectionType={'notes'}
-            subtitle={'Notes'}
-            placeholder={''}
-          />
-          <SaveButton />
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} backgroundColor={"orange"}>
+          <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+            <LessonSection sectionType={'warmUp'} subtitle={'Warm Up'} />
+            <LessonSection sectionType={'mainLesson'} subtitle={'Main Lesson'} />
+            <LessonSection sectionType={'coolDown'} subtitle={'Cool Down'} />
+            <LessonPlanNotes
+              sectionType={'notes'}
+              subtitle={'Notes'}
+              placeholder={''}
+            />
+            <SaveButton />
+          </View>
+        </TouchableWithoutFeedback>
       </ScrollView>
     </SafeAreaView>
   );
