d2ed74a6b510369b65838324caf6d046a07fb4df
ramyzhang <ramyjzh@gmail.com>
2023-02-15 15:19:57 -0500

    fix(lessonplaneditor.js): made touchablewithoutfeedback touch sides of screen

--- src/editor/screens/LessonPlanEditor.js
+++ src/editor/screens/LessonPlanEditor.js
@@ -9,29 +9,29 @@ const LessonPlanEditor = ({ navigation }) => {
   return (
     <SafeAreaView style={{ backgroundColor: '#FFFAF5' }}>
       <LessonPlanHeader />
-      <ScrollView style={styles.scrollView}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} backgroundColor={"orange"}>
-          <View style={{ flexDirection: 'column', alignItems: 'center' }}>
-            <LessonSection sectionType={'warmUp'} subtitle={'Warm Up'} />
-            <LessonSection sectionType={'mainLesson'} subtitle={'Main Lesson'} />
-            <LessonSection sectionType={'coolDown'} subtitle={'Cool Down'} />
-            <LessonPlanNotes
-              sectionType={'notes'}
-              subtitle={'Notes'}
-              placeholder={''}
-            />
-            <SaveButton />
-          </View>
-        </TouchableWithoutFeedback>
-      </ScrollView>
+        <ScrollView style={styles.scrollView}>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss} flex={1}>
+            <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+              <LessonSection sectionType={'warmUp'} subtitle={'Warm Up'} />
+              <LessonSection sectionType={'mainLesson'} subtitle={'Main Lesson'} />
+              <LessonSection sectionType={'coolDown'} subtitle={'Cool Down'} />
+              <LessonPlanNotes
+                sectionType={'notes'}
+                subtitle={'Notes'}
+                placeholder={''}
+              />
+              <SaveButton />
+            </View>
+          </TouchableWithoutFeedback>
+        </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   scrollView: {
-    marginHorizontal: 20,
-    marginVertical: 5,
+    marginHorizontal: '1%',
+    marginVertical: '1%',
     marginBottom: '20%'
   }
 });
