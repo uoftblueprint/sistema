ba9cea363dea95711517be9415054d3187cc93b3
Emily Yu <em.yu@mail.utoronto.ca>
2023-03-06 23:49:07 -0500

    feat(header.js): add scroll to activity card naming overlay, add blurb

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ import {
   View,
   Text,
   TouchableOpacity,
+  ScrollView
 } from 'react-native';
 import SistemaButton from '../Components/SistemaButton';
 import SistemaLogo from '../../assets/SistemaLogo.svg';
@@ -13,8 +14,12 @@ import GearIcon from '../../assets/gearIcon.svg';
 import Overlay from './Overlay';
 import { STACK_SCREENS as SETTINGS_SCREENS } from '../settings/constants';
 
+const activityCardNamingExplanation = 'Activity Card names have three parts:\n\nTheme - Activity Type - Activity Title\n\n' + 
+  'The theme will be the Social or Theory and Musicianship theme the activity was developed for, for example "Identity", "Problem Solving", or Rhythm"\n\n' + 
+  'There are four Activity Types, each describing a different way to approach learning about the social or musical theme: Knowledge, Action, Perception, and Creation.\n\n' +
+  'The Activity Title identifies the specific activity or activities on the card, for example, "Roses and Thorns", or "Naming Intervals". Sometimes the names are jokes or puns, sometimes just a shorthand for what the activity will involve.'
+
 const Header = ({ navigation, isHome }) => {
-  const [isHomePage, setHomePage] = useState(isHome);
   const [isVisible, setVisible] = useState(false);
 
   const toggleOverlay = () => {
@@ -31,24 +36,23 @@ const Header = ({ navigation, isHome }) => {
         onPress={() => navigation.navigate(SETTINGS_SCREENS.NAVIGATOR, { screen: SETTINGS_SCREENS.SETTINGS_MAIN })}>
         <GearIcon width={30} height={30} style={styles.settingIcon} />
       </TouchableOpacity>
-      {isHomePage ? (
+      {isHome && (
         <TouchableOpacity style={styles.infoIcon} onPress={toggleOverlay}>
           <InfoIcon height={37} width={37} />
         </TouchableOpacity>
-      ) : (
-        <></>
       )}
       <Overlay
         close={toggleOverlay}
         visible={isVisible}
         style={styles.overlayStyle}>
         <Text style={styles.textHeader}>How activity cards are named</Text>
-        <Text style={styles.textBody}>x, y, z - Ask Alex to write this</Text>
-        <View style={styles.buttonStyle}>
-          <SistemaButton onPress={toggleOverlay} style={{ minWidth: '30%' }}>
-            <Text> Okay </Text>
-          </SistemaButton>
-        </View>
+        <ScrollView style={styles.overlayScroll}>
+          <Text style={styles.textBody}>{activityCardNamingExplanation}</Text>
+        </ScrollView>
+        
+        <SistemaButton onPress={toggleOverlay} style={{ minWidth: '30%' }}>
+          <Text> Okay </Text>
+        </SistemaButton>
       </Overlay>
     </SafeAreaView>
   );
@@ -85,24 +89,23 @@ const styles = StyleSheet.create({
   },
   textHeader: {
     fontFamily: 'Poppins-Bold',
+    fontSize: 16,
     marginBottom: '5%',
   },
   textBody: {
     fontFamily: 'Mulish-Regular',
+    fontSize: 16,
     marginBottom: '5%',
   },
   overlayStyle: {
     minHeight: '30%',
+    maxHeight: '60%',
     paddingHorizontal: '5%',
+    flexDirection: 'column'
   },
-  buttonStyle: {
-    position: 'absolute',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    left: 0,
-    right: 0,
-    top: '95%',
-  },
+  overlayScroll: {
+    marginBottom: '5%'
+  }
 });
 
 export default Header;
