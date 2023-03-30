4f03a2e8d31fcd379260abbf294b1a8e5cc53d90
Emily Yu <em.yu@mail.utoronto.ca>
2023-03-29 20:41:29 -0400

    refactor: add file to ignore non-breaking warnings and error logs

---
+++ src/services/ignoreWarnings.js
@@ -0,0 +1,13 @@
+if (__DEV__) {
+  const ignoreWarns = ["VirtualizedLists should never be nested inside plain ScrollViews"];
+
+  const errorWarn = global.console.error;
+  global.console.error = (...arg) => {
+    for (const error of ignoreWarns) {
+      if (arg[0].startsWith(error)) {
+        return;
+      }
+    }
+    errorWarn(...arg);
+  };
+}
