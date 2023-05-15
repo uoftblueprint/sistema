1a13bc3bfea249b4b021f0e1fdab5baa0caf151f
Emily Yu <em.yu@mail.utoronto.ca>
2023-05-15 01:17:03 -0600

    feat: add new redux store slice to store recent AC names

--- src/services/configureStore.js
+++ src/services/configureStore.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './login/authSlice';
 import lessonPlanReducer from './editor/lessonPlanSlice';
+import recentActivityCardsReducer from './editor/recentActivityCardsSlice';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     lessonPlan: lessonPlanReducer,
+    recentActivityCards: recentActivityCardsReducer
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
