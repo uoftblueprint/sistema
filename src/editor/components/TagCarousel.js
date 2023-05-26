8dc978edb7b225c522dfdedf241e6eee85cad7da
yuemily24 <emyueen@gmail.com>
2023-05-25 21:12:01 -0600

    tag carousel

--- src/editor/components/TagCarousel.js
+++ src/editor/components/TagCarousel.js
@@ -31,63 +31,65 @@ const TagCarousel = ({
 
   return (
     <View style={{ flexDirection: 'column' }}>
-      <View style={{ backgroundColor: 'red' }}>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          onScroll={handleScroll}
-          scrollEventThrottle={16}
-          nestedScrollEnabled={true}>
-          {tagsList.map((tag, index) => (
-            <TagFilter
-              key={index}
-              tagContent={tag}
-              active={activeTags[index]}
-              onPress={() => {
-                // make a copy of the active tags
-                let newActiveTags = activeTags.slice();
-                if (
-                  selectOnlyOne &&
-                  !activeTags[index] && // if planning to activate tag
-                  activeTags.includes(true) // and other tags are already active
-                ) {
-                  // turn off all tags since we can only have one active
-                  newActiveTags = new Array(activeTags).fill(false);
-                }
-                // toggle current tag
-                newActiveTags[index] = !newActiveTags[index];
-                setActiveTags(newActiveTags);
-              }}
-            />
-          ))}
-        </ScrollView>
-        {showBottomScrollbar && 
-          <View>
-            <View
-              style={{
-                width: '100%',
-                borderBottomWidth: 1.5,
-                borderBottomColor: '#C7BCBC87',
-                position: 'absolute',
-                top: '130%',
-              }}
-            />
-            <Animated.View
-              style={[
-                styles.segment,
-                {
-                  left: scrollBarPositionPercentage,
-                },
-              ]}
-            />
-          </View>
-        }
-      </View>
+      <ScrollView
+        style={styles.carousel}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
+        nestedScrollEnabled={true}>
+        {tagsList.map((tag, index) => (
+          <TagFilter
+            key={index}
+            tagContent={tag}
+            active={activeTags[index]}
+            onPress={() => {
+              // make a copy of the active tags
+              let newActiveTags = activeTags.slice();
+              if (
+                selectOnlyOne &&
+                !activeTags[index] && // if planning to activate tag
+                activeTags.includes(true) // and other tags are already active
+              ) {
+                // turn off all tags since we can only have one active
+                newActiveTags = new Array(activeTags).fill(false);
+              }
+              // toggle current tag
+              newActiveTags[index] = !newActiveTags[index];
+              setActiveTags(newActiveTags);
+            }}
+          />
+        ))}
+      </ScrollView>
+      {showBottomScrollbar && 
+        <>
+          <View
+            style={{
+              width: '100%',
+              borderBottomWidth: 1.5,
+              borderBottomColor: '#C7BCBC87',
+              position: 'absolute',
+              top: '130%',
+            }}
+          />
+          <Animated.View
+            style={[
+              styles.segment,
+              {
+                left: scrollBarPositionPercentage,
+              },
+            ]}
+          />
+        </>
+      }
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  carousel: {
+    marginBottom: 15,
+  },
   segment: {
     width: '40%',
     borderBottomWidth: 1.5,
