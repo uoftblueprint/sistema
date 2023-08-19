e2d1116e241987e31cfa4f4b851d33658fd3c49e
yuemily24 <76456096+yuemily24@users.noreply.github.com>
2023-08-19 13:19:43 -0600

    [SIS-164] Fix link to Sistema website in About page in Settings  (#164)
    * fix(about page in settings): fix link to sistema website
    
    * fix: give up cleaning and fix link directly
    
    * fix: unused variable

--- src/settings/components/LinkButton.js
+++ src/settings/components/LinkButton.js
@@ -3,17 +3,8 @@ import { StyleSheet, Text, TouchableOpacity, Linking } from 'react-native';
 import { TextStyle } from '../../Styles.config';
 
 const LinkButton = ({ title, url }) => {
-  const handlePress = useCallback(async () => {
-    // Checking if the link is supported for links with custom URL scheme.
-    const supported = await Linking.canOpenURL(url);
-
-    if (supported) {
-      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-      // by some browser in the mobile
-      await Linking.openURL(url);
-    } else {
-      // console.error(`Don't know how to open this URL: ${url}`);
-    }
+  const handlePress = useCallback(() => {
+    Linking.openURL(url);
   }, [url]);
 
   return (
